74ea6bf34cfaa3f405b51ab6cf92ad03f9fcc177
Simran <ss1043422@gmail.com>
2024-11-25 11:58:13 +0530

    Graphs

---
+++ day22/Implement1.java
@@ -0,0 +1,26 @@
+package day22;
+
+public class Implement1 {
+
+	public static void main(String[] args) {
+		graphs1 g = new graphs1(7);
+		g.AddEdge(1, 4, 6);
+		g.AddEdge(1, 2, 10);
+		g.AddEdge(2, 3, 7);
+		g.AddEdge(3, 4, 5);
+		g.AddEdge(4, 5, 1);
+		g.AddEdge(5, 6, 4);
+		g.AddEdge(7, 5, 2);
+		g.AddEdge(6, 7, 3);
+//		 g.display();
+		// System.out.println(g.noofEdge());
+		// g.removeEdge(4, 5);
+		// g.removevertex(4);
+		// g.display();
+		// System.out.println(g.hashpath(1, 6, new HashSet<>()));
+		// g.printallpath(1,  6, new HashSet<>(), "");
+
+		
+	}
+
+}
