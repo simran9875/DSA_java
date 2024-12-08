c2b471a6afc661bde735dcab7b8dbb63f7c449ba
Simran <ss1043422@gmail.com>
2024-12-08 11:46:12 +0530

    Merge Sort

--- day11/MergeSort.java
+++ day11/MergeSort.java
@@ -39,7 +39,6 @@ public class MergeSort {
         }
     }
     public static void main(String[] args) {
-    	
         int arr[]={1,4,2,3,7,6,8};
         int low=0;
         int high= arr.length-1;
